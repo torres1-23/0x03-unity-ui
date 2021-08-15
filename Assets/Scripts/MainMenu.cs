7207f32de26e1f2c55614165dd8b2bc7363e2a38
Alejandro Torres <alejo.torres1247@gmail.com>
2021-08-15 17:56:30 -0500

    Add color blind mode option

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,26 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    /// <summary>PlayButton on click loads the maze scene </summary>
+    // ColorBlind and normal scene mode variables
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    /// <summary>PlayButton on click loads the maze scene 
+    /// if Color Blind Toggle is on game renders in colorBlind mode</summary>
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
+      
         SceneManager.LoadScene("maze", LoadSceneMode.Single);
     }
 

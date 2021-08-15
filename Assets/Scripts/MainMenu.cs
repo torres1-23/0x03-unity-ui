91395ff9d602d8a89adc789aaec71bc803dfe345
Alejandro Torres <alejo.torres1247@gmail.com>
2021-08-15 12:35:14 -0500

    Add main menu quit button behaviour

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,16 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    /// <summary>PlayButton on click load the maze scene </summary>
+    /// <summary>PlayButton on click loads the maze scene </summary>
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze", LoadSceneMode.Single);
+    }
+
+    /// <summary>QuitButton on click quits game</summary>
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
